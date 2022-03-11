02d7c27883eafd14e0408bad707f39137392a5cf
Bakdaulet Ye <b.yelmurzayev@gmail.com>
2022-03-12 02:35:29 +0600

    id

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ dotenv.config()
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.inlineQuery('a', (ctx) => {
-    let apchuSize = getRandomInt(3, 40)
-    let answer = `Сегодня ты дал ${apchuSize} сантиметровый апчу`
     let id = 0
+
+    let apchuSize = getRandomInt(3, 40)
+    let answer = `Сегодня ты дал ${apchuSize} сантиметровый апчу id = ${id}`
 
     ctx.answerInlineQuery([
       {
