bd0dfc75a70633173051748d3ba12b403d6a6cb2
Bakdaulet Ye <b.yelmurzayev@gmail.com>
2022-03-12 02:32:25 +0600

    asd

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ import {getRandomInt} from './randomInt.js'
 dotenv.config()
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.inlineQuery(['a'], (ctx) => {
+bot.inlineQuery('a', (ctx) => {
     let apchuSize = getRandomInt(3, 40)
     let answer = `Сегодня ты дал ${apchuSize} сантиметровый апчу`
-  
+    let id = 0
+
     ctx.answerInlineQuery([
       {
-        id: '1',
+        id: `${id}`,
         type: 'article',
         title: 'Апщу бер',
         input_message_content: {
@@ -20,6 +21,8 @@ bot.inlineQuery(['a'], (ctx) => {
         description: 'Покажет, насколько большой у тебя апщу'
       }      
     ])
+
+    id++
   })
 
 bot.launch()
