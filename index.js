36d43cdf5fbff197a1a9fb59178e471e48823597
Bakdaulet Ye <b.yelmurzayev@gmail.com>
2022-03-12 17:12:01 +0600

    no cache time

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ bot.on('inline_query', (ctx) => {
         },
         description: 'Покажет, насколько большой у тебя апщу',
       } 
-    ], { is_personal: true, cache_time: 43200 })
+    ], { is_personal: true, cache_time: 1 })
   })
 
 bot.launch()
