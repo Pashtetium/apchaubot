d3e625279961da8d3e62dab29c9b0e617f8d839f
Bakdaulet Ye <b.yelmurzayev@gmail.com>
2023-03-27 17:04:44 +0600

    Added vip users

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
-import dotenv from 'dotenv'
-import { Telegraf } from 'telegraf'
-import { getApchuSize } from './apchuSize.js'
-import { getEmoji } from './emoji.js'
+import dotenv from "dotenv";
+import { Telegraf } from "telegraf";
+import { getApchuSize } from "./apchuSize.js";
+import { getEmoji } from "./emoji.js";
+import { vipList } from "./vip-list.js";
 
-dotenv.config()
-const bot = new Telegraf(process.env.BOT_TOKEN)
+dotenv.config();
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.on('inline_query', (ctx) => {
-    let apchuSize = getApchuSize()
-    console.log('apchuSize = ' + apchuSize)
-    let emoji = getEmoji(apchuSize)
-    console.log('emoji = ' + emoji)
+bot.on("inline_query", (ctx) => {
+  let apchuSize = getApchuSize();
+  const emoji = getEmoji(apchuSize);
+  const isVipKazakh =
+    vipList.find((vip) => ctx.from?.username === vip)?.length > 0;
 
-    let answer = `Сегодня ты дал Апщу на ${apchuSize}см. ${emoji}`
-    console.log('answer = ' + answer)
-    ctx.answerInlineQuery([      
+  if (isVipKazakh) {
+    apchuSize += 10;
+  }
+
+  const answer = `Сегодня ты дал Апщу на ${apchuSize}см. ${
+    isVipKazakh ? "😎(vip)😎" : emoji
+  }`;
+
+  ctx.answerInlineQuery(
+    [
       {
-        id: '1',
-        type: 'article',
-        title: 'Апщу бер',
+        id: "1",
+        type: "article",
+        title: "Апщу бер",
         input_message_content: {
           message_text: answer,
         },
-        description: 'Покажет, насколько большой у тебя апщу',
-      } 
-    ], { is_personal: true, cache_time: 43200})
-  })
+        description: "Покажет, насколько большой у тебя апщу",
+      },
+    ],
+    { is_personal: true, cache_time: 43200 }
+  );
+});
 
-bot.launch()
+bot.launch();
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
---
+++ vip-list.js
@@ -0,0 +1,8 @@
+export const vipList = [
+  "donraul0207",
+  "pashtetium",
+  "Myrzakhmet1",
+  "sugarbeiby",
+  "kavelpim123",
+  "ddarzox",
+];
