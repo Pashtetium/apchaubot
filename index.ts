cf354b8115f4f14b0014286c35c9e2215a08ec66
Bakdaulet Ye <b.yelmurzayev@gmail.com>
2023-11-17 16:29:51 +0600

    Stats
    * feat: added basic stats

--- index.ts
+++ index.ts
@@ -1,43 +1,96 @@
-import dotenv from "dotenv";
-import { Telegraf } from "telegraf";
+import { Telegraf, Context } from "telegraf";
 import { getApchuSize } from "./apchuSize.js";
 import { getEmoji } from "./emoji.js";
-import { vipList } from "./vip-list.js";
+import { isVipUser } from "./vip-list.js";
+import { MongoDbDriver, Stats } from "./storage/mongodb/mongoDbDriver.js";
 
-dotenv.config();
-const bot = new Telegraf(process.env.BOT_TOKEN ?? "");
+async function main() {
+  const connectionString = process.env.MONGODB_CONNECTION_STRING;
 
-bot.on("inline_query", (ctx) => {
-  let apchuSize = getApchuSize();
-  const emoji = getEmoji(apchuSize);
-  const isVipKazakh = !!vipList[ctx.from?.id];
-
-  if (isVipKazakh) {
-    apchuSize += 10;
+  if (!process.env.BOT_TOKEN || !connectionString) {
+    throw new Error("Env variables not found");
   }
 
-  const answer = `Сегодня ты дал Апщу на ${apchuSize}см. ${
-    isVipKazakh ? "😎(vip)😎" : emoji
-  }`;
+  const mongo = new MongoDbDriver(connectionString);
 
-  ctx.answerInlineQuery(
-    [
-      {
-        id: "1",
-        type: "article",
-        title: "Апщу бер",
-        input_message_content: {
-          message_text: answer,
+  try {
+    await mongo.openConnection();
+  } catch (e) {
+    console.error(e);
+  }
+
+  const bot = new Telegraf(process.env.BOT_TOKEN);
+
+  bot.on("inline_query", async (ctx: Context) => {
+    let apchuSize = getApchuSize();
+    const emoji = getEmoji(apchuSize);
+    const isVipKazakh = isVipUser(ctx.from?.id);
+
+    if (isVipKazakh) {
+      apchuSize += 10;
+    }
+
+    const answer = `Сегодня ты дал Апщу на ${apchuSize}см. ${
+      isVipKazakh ? "😎(vip)😎" : emoji
+    }`;
+
+    const stats: Stats = {
+      userId: ctx.from?.id,
+      firstName: ctx.from?.first_name,
+      lastName: ctx.from?.last_name,
+      userName: ctx.from?.username,
+      apchuSize,
+    };
+
+    try {
+      const savedStats = await mongo.saveStats(stats);
+      console.info(savedStats);
+    } catch (e) {
+      console.error(e);
+    }
+
+    const averageSize = await mongo.getAverageSizeForUser(ctx.from?.id);
+
+    const statsAnswer = `Твой средний размер за всё время - ${getEmoji(
+      averageSize
+    )}`;
+
+    ctx.answerInlineQuery(
+      [
+        {
+          id: "1",
+          type: "article",
+          title: "Апщу бер",
+          input_message_content: {
+            message_text: answer,
+          },
+          description: "Покажет, насколько большой у тебя апщу",
         },
-        description: "Покажет, насколько большой у тебя апщу",
-      },
-    ],
-    { is_personal: true, cache_time: 43200 }
-  );
-});
+        {
+          id: "2",
+          type: "article",
+          title: "Твоя статистика",
+          input_message_content: {
+            message_text: statsAnswer,
+          },
+          description: "Покажет твой средний размер",
+        },
+      ],
+      { is_personal: true, cache_time: 43200 }
+    );
+  });
 
-bot.launch();
+  bot.launch();
 
-// Enable graceful stop
-process.once("SIGINT", () => bot.stop("SIGINT"));
-process.once("SIGTERM", () => bot.stop("SIGTERM"));
+  // Enable graceful stop
+  process.once("SIGINT", async () => {
+    await mongo.closeConnection();
+    bot.stop("SIGINT");
+  });
+  process.once("SIGTERM", async () => {
+    await mongo.closeConnection();
+    bot.stop("SIGTERM");
+  });
+}
+
+main();
--- vip-list.ts
+++ vip-list.ts
@@ -1,19 +1,27 @@
-interface VipList {
-  [userId: number]: string;
+interface VipUsers {
+  [key: number]: string;
 }
 
-export const vipList: VipList = {
-  187544508: "donraul0207",
-  986068685: "pashtetium",
-  819445530: "Myrzakhmet1",
-  1866151537: "sugarbeiby",
-  998133479: "kavelpim123",
-  390057766: "ddarzox",
-  452389199: "acoool",
-  1244303750: "biqontie",
-  607477219: "DihanB",
-  395014965: "A3300532",
-  586849836: "SagnaAlvarez",
-  970301418: "maskoffffff",
-  1658306772: "MrRobotDumbazz",
-};
+export function isVipUser(id: number | undefined): boolean {
+  if (!id) {
+    return false;
+  }
+
+  const users: VipUsers = {
+    187544508: "donraul0207",
+    986068685: "pashtetium",
+    819445530: "Myrzakhmet1",
+    1866151537: "sugarbeiby",
+    998133479: "kavelpim123",
+    390057766: "ddarzox",
+    452389199: "acoool",
+    1244303750: "biqontie",
+    607477219: "DihanB",
+    395014965: "A3300532",
+    586849836: "SagnaAlvarez",
+    970301418: "maskoffffff",
+    1658306772: "MrRobotDumbazz",
+  };
+
+  return !!users[id];
+}
